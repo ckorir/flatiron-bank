09d22a6dd66bc5838715c70fa8b932347295ea8e
ckorir <chrisruto.k@gmail.com>
2023-10-30 12:26:36 +0300

    Added code for Transaction Table

--- src/components/TransactionTable.js
+++ src/components/TransactionTable.js
@@ -1,7 +1,24 @@
 import React from 'react'
 
-export default function TransactionTable() {
-  return (
-    <div>TransactionTable</div>
-  )
+export default function TransactionTable({ transactions }) {
+    return (
+        <table>
+          <thead>
+            <tr>
+              <th>Date</th>
+              <th>Description</th>
+              <th>Amount</th>
+            </tr>
+          </thead>
+          <tbody>
+            {transactions.map((transaction) => (
+              <tr key={transaction.id}>
+                <td>{transaction.date}</td>
+                <td>{transaction.description}</td>
+                <td>{transaction.amount}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      );
 }
